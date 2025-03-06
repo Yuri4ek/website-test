f97a00dd3a0db230b6baa509ac4edbd6ffc2367b
Yuri4ek <dinourski@gmail.com>
2025-03-06 22:48:17 +0300

    Добавил вывод по сортировке

--- test programm/functions.py
+++ test programm/functions.py
@@ -1,11 +1,26 @@
 import sqlite3
 
+tablesNames = ['motherboards', 'processors',
+               'videocards', 'rammodules',
+               'storagedevices', 'coolingsystems',
+               'powersupplies', 'computercases']
+
 
 def getValuesFromTable(cursor, tableName):
-    request = f"SELECT * from {tableName}"
+    request = f"SELECT * FROM {tableName}"
     cursor.execute(request)
     return cursor.fetchall()
 
 
-def getSortedValuesFromTable(cursor, tableName, key):
-    pass
+def getSortedValuesFromTable(cursor, tableName, mainKey):
+    request = f"SELECT * FROM {tableName} "
+
+    request += "WHERE "
+    for i, obj in enumerate(mainKey):
+        key, value = obj
+        if i > 0:
+            request += " AND "
+        request += f"{key} == '{value}'"
+
+    cursor.execute(request)
+    return cursor.fetchall()
--- test programm/main.py
+++ test programm/main.py
@@ -3,14 +3,25 @@ from functions import *
 
 DBpath = '../db files/components.db'
 
-connectino = sqlite3.connect(DBpath)
-cursor = connectino.cursor()
+connection = sqlite3.connect(DBpath)
+cursor = connection.cursor()
+
+print('Начало')
 
 '''
     Основной код тут
 '''
-pprint(getValuesFromTable(cursor, "processors"))
+'''pprint(getSortedValuesFromTable(cursor, 'processors',
+                                [['socket', 'FCLGA1851']]))'''
 
-connectino.close()
+print("Напишите что-нибудь для начала")
+while (command := input()) != "stop":
+    print("Напишите 'stop' для остановки")
+    print("Напишите 'print <название таблицы> <ключ (по необходимости)>' "
+          "для вывода таблицы")
+    print("Напишите 'get <название таблицы> <id комплектующего> "
+          "для выбора комплектующего'")
 
-print('Начало')
+connection.close()
+
+print('Конец')
