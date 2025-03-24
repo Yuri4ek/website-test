e0220027bf780f018d6ec914c6951c9251c177a2
Yuri4ek <dinourski@gmail.com>
2025-03-24 22:34:15 +0300

    добавил таблицу

--- main.py
+++ main.py
@@ -16,5 +16,21 @@ def home():
     return render_template('main.html', user=user)
 
 
+@app.route('/table')
+def show_table():
+    # Пример данных
+    table_headers = ["ID", "Имя", "Возраст"]
+    table_data = [
+        [1, "Алексей", 25],
+        [2, "Мария", 30],
+        [3, "Иван", 22]
+    ]
+
+    return render_template('table.html',
+                           user=user,
+                           headers=table_headers,
+                           data=table_data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
